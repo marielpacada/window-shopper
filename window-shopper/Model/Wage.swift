9fe4153964be89938dab2255b0160fd91b8a629e
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-01-15 14:30:17 -0800

    test

--- window-shopper/Model/Wage.swift
+++ window-shopper/Model/Wage.swift
@@ -10,8 +10,8 @@ import Foundation
 class Wage {
     
     class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
-        
-        return Int(ceil(price / wage))
+        let wage = Int(wage)
+        return Int(ceil(price / Double(wage)))
         
         
         
