29e665693f15ac1ae4c2700ff6bda8e482655f6a
marielpacada <marielpacada@Mariels-MBP.fios-router.home>
2021-01-15 14:12:12 -0800

    testing

---
+++ window-shopper/Model/Wage.swift
@@ -0,0 +1,19 @@
+//
+//  Wage.swift
+//  window-shopper
+//
+//  Created by Mariel Pacada on 1/15/21.
+//
+
+import Foundation
+
+class Wage {
+    
+    class func getHours(forWage wage: Double, andPrice price: Double) -> Int {
+        
+        return Int(ceil(price / wage))
+        
+        
+        
+    }
+}
